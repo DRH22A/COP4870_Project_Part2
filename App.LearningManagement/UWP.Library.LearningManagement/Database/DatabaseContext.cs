bb433b0b93c29f7bb20f7d5b2edf03d8b9fe28c3
Daniel Halterman <olienad@yahoo.com>
2023-04-28 18:04:02 -0400

    added a database and connected my project to my.NET API project using the dll file

---
+++ App.LearningManagement/UWP.Library.LearningManagement/Database/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Library.LearningManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace UWP.Library.LearningManagement.Database
+{
+    public static class DatabaseContext
+    {
+        public static ObservableCollection<Course> courses = new ObservableCollection<Course>
+        {
+            new Course { Code = "COP1212", Name = "COMPUTING", Description = "COOL COMPUTERS", Room = "HCB103", Semester = { Season = SeasonEnum.Spring, Year = YearEnum.Year_2023}}
+        };
+    }
+}
